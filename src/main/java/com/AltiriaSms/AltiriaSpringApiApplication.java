c247124717568efcb47f63db8aa3c719f30dccdb
EstebanNavas <arcoesteb@live.com>
2023-06-30 19:35:04 -0500

    Se crean conexiones a las bases de datos bdaquamovil y BDMailMarketing

---
+++ src/main/java/com/AltiriaSms/AltiriaSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.AltiriaSms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltiriaSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltiriaSpringApiApplication.class, args);
+	}
+
+}
